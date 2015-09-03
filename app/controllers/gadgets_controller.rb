ceeb6701ce227096aedff2725047af5b58f354cf
Solotchi Veaceslav <slawa85@gmail.com>
2015-09-03 10:35:52 +0300

    Gadget controller with index/create/new methods

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,22 +1,45 @@
 class GadgetsController < ApplicationController
+  before_action :set_gadget, only: [:show, :edit, :update, :destroy]
+
   def index
+    @gadgets = Gadget.get_by current_user.to_param
   end
 
   def show
   end
 
   def new
+    @gadget = Gadget.new
   end
 
   def edit
   end
 
   def create
+    @gadget = Gadget.new(gadget_params)
+
+    respond_to do |format|
+      if @gadget.save
+        format.html { redirect_to @gadget, notice: 'Gadget was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_gadget
+    @gadget = Gadget.find(params[:id])
+  end
+
+  def gadget_params
+    params.require(:gadget).permit(:name, :description, :user_id)
   end
 end
--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,3 +1,7 @@
 class Gadget < ActiveRecord::Base
   validates :name, presence: true,  length: { minimum: 3 }
+
+  def self.get_by(owner_id)
+    owner_id.nil? ? all : where("user_id = #{owner_id}")
+  end
 end
